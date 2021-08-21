9c6c3fab80df8175328196afb4b507932073559e
ben <benjamin.minel@outlook.fr>
2021-08-21 14:10:06 +0200

    carousel about me view personal

--- lib/about_me/about_me_view.dart
+++ lib/about_me/about_me_view.dart
@@ -1,15 +1,168 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mdi/mdi.dart';
 import 'package:portofolio/core/widget/x_scaffold.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'about_me_view_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class AboutMeView extends GetView<AboutMeViewController> {
   @override
   Widget build(BuildContext context) {
     return XScaffold(
-      child: Center(),
+      child: _buildContent(context),
       indexNavigation: 1,
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              width: context.width,
+              child: Column(
+                children: [
+                  Text('Minel Benjamin'),
+                  Text('job'.tr),
+                ],
+              ),
+            ),
+            Container(
+              width: context.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, bottom: 20),
+                    child: Text(
+                      'myPersonalProject'.tr,
+                      style: Get.textTheme.headline2,
+                    ),
+                  ),
+                  CarouselSlider(
+                    items: [
+                      _buildCarousel(
+                        'assets/raspberry_pi_4.png',
+                        'presentationRaspberryPiProjet'.tr,
+                        context,
+                      ),
+                      _golangCarousel(context),
+                      _buildCarousel(
+                        'assets/pi_hole.png',
+                        'presentationPiHoleProjet'.tr,
+                        context,
+                        height: context.height * 0.2,
+                      ),
+                      _buildCarousel(
+                        'assets/mame.png',
+                        'presentationMameProjet'.tr,
+                        context,
+                      ),
+                    ],
+                    options: CarouselOptions(
+                      height: 400,
+                      viewportFraction: 0.45,
+                      initialPage: 4,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 5),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCarousel(
+    String pathPicture,
+    String textPresentation,
+    BuildContext context, {
+    double? height,
+  }) {
+    return Container(
+      color: Get.theme.accentColor,
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Image.asset(
+              pathPicture,
+              fit: BoxFit.scaleDown,
+              height: height,
+              width: context.width * 0.2,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    textPresentation,
+                    style: Get.textTheme.bodyText1!
+                        .copyWith(color: Get.theme.backgroundColor),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _golangCarousel(BuildContext context) {
+    return Container(
+      color: Get.theme.accentColor,
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Image.asset(
+              'assets/go.png',
+              fit: BoxFit.contain,
+              width: context.width * 0.2,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    'presentationGoLangProjet'.tr,
+                    style: Get.textTheme.bodyText1!
+                        .copyWith(color: Get.theme.backgroundColor),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    launch(
+                      'https://github.com/ectrema/translation_api',
+                    );
+                  },
+                  child: Icon(
+                    Mdi.github,
+                    color: Get.theme.backgroundColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/core/translation/fr_FR.dart
+++ lib/core/translation/fr_FR.dart
@@ -3,8 +3,17 @@ final Map<String, String> frFR = {
   'home': 'Accueil',
   'aboutMe': 'À propos de moi',
   'job': 'Développeur d\'application mobile',
-  'resume' : 'Mes projets, mes expériences, mon parcours...',
-  'project' : 'Mes projets',
-  'network' : 'Réseau sociaux',
-  'myDescription': 'Bonjour, Je suis Minel Benjamin, un jeune développeur qui se spécialise dans le donmaine du développement mobile.\nVous Trouverez ici ma présentation ainsi que quelques projets que j\'ai réalisé.'
+  'resume': 'Mes projets, mes expériences, mon parcours...',
+  'project': 'Mes projets',
+  'network': 'Réseau sociaux',
+  'myDescription':
+      'Bonjour, Je suis Minel Benjamin, un jeune développeur qui se spécialise dans le donmaine du développement mobile.\nVous trouverez ici ma présentation ainsi que quelques projets que j\'ai réalisé.',
+  'presentationGoLangProjet':
+      'Faire une Api en Golang pour récuperer les traductions de ce site web depuis une bdd sotcker dans un docker sur mon raspberry pi.',
+  'presentationRaspberryPiProjet':
+      'Configuration d\'un Raspberry Pi 4 pour des projets persos (Ex: serveur, Api, docker, station de jeux rétro...)',
+  'presentationPiHoleProjet':
+      'Installation et configuration de pi-hole dans un conteneur docker dans le raspberry pi',
+  'presentationMameProjet': 'Installation et configuration de Mame pour jouer a des jeux rétros sur le raspberry pi. (Si vous avez des Roms je suis preneur Merci!)',
+  'myPersonalProject': 'Mes projets personnels',
 };
