e417541bcb1f22ae45ae62fc1703ee81e2669605
ben <benjamin.minel@outlook.fr>
2021-08-28 12:21:36 +0200

    skills part

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,172 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:mdi/mdi.dart';
 import 'package:portofolio/core/widget/x_bottom_sheet.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:portofolio/home/widget/main_widget.dart';
+import 'package:portofolio/home/widget/skills_widget.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: _buildContent(context),
-      bottomSheet: XBottomSheet(),
+      // bottomSheet: XBottomSheet(),
     );
   }
 
   Widget _buildContent(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: _buildHeader(context),
-              ),
-              _buildMain(context),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildHeader(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 100),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            'Portofolio',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Row(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Theme.of(context).primaryColor,
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding: const EdgeInsets.only(top: 20),
-                child: Center(
-                  child: Text(
-                    AppLocalizations.of(context)!.about,
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding: const EdgeInsets.only(top: 20),
-                child: Center(
-                  child: Text(
-                    'portofolio',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding: const EdgeInsets.only(top: 20),
-                child: Center(
-                  child: Text(
-                    'contact',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMain(BuildContext context) {
-    return Container(
-      // height: context.height,
-      // width: context.width,
-      margin: const EdgeInsets.only(left: 100),
+    return SingleChildScrollView(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Bonjour je suis',
-            style: Theme.of(context).textTheme.headline4,
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: MainWidget(),
           ),
-          Text(
-            'Benjamin Minel',
-            style: Theme.of(context).textTheme.headline3,
+          SizedBox(
+            height: MediaQuery.of(context).size.height - 80,
+            child: SkillsWidget(),
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 5, bottom: 15),
-            width: 700,
-            child: Text(
-              AppLocalizations.of(context)!.myDescription,
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(
-                    AppLocalizations.of(context)!.network + ' :',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: InkWell(
-                    onTap: () {
-                      Uri emailLaunchUri = Uri(
-                        scheme: 'mailto',
-                        path: 'benjamin.minel@outlook.fr',
-                      );
-                      launch(emailLaunchUri.toString());
-                    },
-                    child: Text(
-                      'benjamin.minel@outlook.fr',
-                      style: Theme.of(context).textTheme.headline5,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: InkWell(
-                    onTap: () {
-                      launch(
-                        'https://github.com/ectrema/portofolio',
-                      );
-                    },
-                    child: Icon(Mdi.github),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: InkWell(
-                    onTap: () {
-                      launch(
-                        'https://www.linkedin.com/in/benjamin-minel-96435a181/',
-                      );
-                    },
-                    child: Icon(Mdi.linkedin),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          XBottomSheet(),
         ],
       ),
     );
---
+++ lib/home/widget/skills_widget.dart
@@ -0,0 +1,116 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+class SkillsWidget extends StatelessWidget {
+  const SkillsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            AppLocalizations.of(context)!.mySkills,
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: 500,
+            child: Divider(
+              thickness: 3,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          Text(
+            AppLocalizations.of(context)!.mySkillsSubtitle,
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          const Expanded(child: SizedBox()),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    'flutter.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'cpp.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'c.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'golang.png',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(child: SizedBox()),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    'docker.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'github.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'jira.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'sql.png',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(child: SizedBox()),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    'linux.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'laravel.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'java.png',
+                  ),
+                ),
+                Expanded(
+                  child: Image.asset(
+                    'apple.png',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
